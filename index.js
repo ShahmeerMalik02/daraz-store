0745ea20ac1495743be6e9fcc17d81aa353d7e15
shahmeermalik02 <shahmeermalik259@gmail.com>
2023-04-14 00:15:09 +0500

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+let firstname = "WELLCOME TO MY STORE"; // variables // var // let
+//variables rules
+// 1. legal or illegal names
+// use camelcase for long variables name
+// 1. legal 
+let lastNmae // this is starting capital letter example N // camelcase
+
+// illegal
+// spaces
+
+
+
+alert(firstname);
